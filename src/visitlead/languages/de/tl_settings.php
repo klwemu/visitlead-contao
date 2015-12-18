69f456383987dc36231e54ae850c9256b97cc713
klwemu <klwemu@gmail.com>
2015-12-18 14:20:41 +0100

    spelling fix

--- src/visitlead/languages/de/tl_settings.php
+++ src/visitlead/languages/de/tl_settings.php
@@ -1,4 +1,4 @@
 <?php	
 	
 $GLOBALS['TL_LANG']['tl_settings']['visitlead_settings'] = 'Visitlead Einstellungen';
-$GLOBALS['TL_LANG']['tl_settings']['visitlead_id'] = array('Visitlead ID', 'Geben Sie Ihre Visitlead ID ein, der JavaScript Code wird dann automatisch im Quellcode eingefügt.');
+$GLOBALS['TL_LANG']['tl_settings']['visitlead_id'] = array('Visitlead CID', 'Geben Sie Ihre Visitlead CID ein, der JavaScript Code wird dann automatisch im Quellcode eingefügt.');
--- src/visitlead/languages/en/tl_settings.php
+++ src/visitlead/languages/en/tl_settings.php
@@ -1,4 +1,4 @@
 <?php	
 	
 $GLOBALS['TL_LANG']['tl_settings']['visitlead_settings'] = 'Visitlead Settings';
-$GLOBALS['TL_LANG']['tl_settings']['visitlead_id'] = array('Visitlead ID', 'Please put in your Vistalead ID, the necessary script will be added to your Site by our module.');
+$GLOBALS['TL_LANG']['tl_settings']['visitlead_id'] = array('Visitlead CID', 'Please put in your Vistlead CID, the necessary script will be added to your Site by our module.');
