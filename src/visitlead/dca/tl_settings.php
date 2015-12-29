97338c987858397e32ffe77b870a431dc15d24f5
klwemu <klwemu@gmail.com>
2015-12-29 15:57:28 +0100

    minor fixings

--- src/visitlead/dca/tl_settings.php
+++ src/visitlead/dca/tl_settings.php
@@ -1,12 +1,5 @@
 <?php
 
-// User: Thorsten
-// IDE: PhpStorm
-// Copyright LHAW.DEVELOPMENT UG (haftungsbeschränkt)
-// Auf der Eich 40
-// 56727 Mayen
-// Author: Thorsten
-
 $overlay = "{visitlead_settings},visitlead_id";
 $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ";$overlay;";
 $GLOBALS['TL_DCA']['tl_settings']['fields']['visitlead_id'] = array
